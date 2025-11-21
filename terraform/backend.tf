fd5cd8e3c5cda30d1108261ab0f6ca13643ea12a
Maksim Skobun <max@skobun.me>
2025-11-22 01:02:39 +0800

    backend.tf: Use S3 native lock instead of deprecated dynamodb one

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "eco-water-temps-tf-state-240232487020"
-    key            = "terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "eco-water-temps-tf-lock"
-    encrypt        = true
+    bucket       = "eco-water-temps-tf-state-240232487020"
+    key          = "terraform.tfstate"
+    region       = "us-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }
