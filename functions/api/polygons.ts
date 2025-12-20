bdd1d805b04889beb1916dc7faa94ec072870849
Maksim Skobun <max@skobun.me>
2025-12-20 17:25:00 +0800

    Remove raw cloudflare functions
    They have been migrated to sveltekit

--- functions/api/polygons.ts
+++
@@ -1,6 +0,0 @@
-import { getObject, type Env } from './_shared';
-
-export async function onRequest(context: { env: Env }): Promise<Response> {
-  return getObject(context.env, "static/polygons_new.geojson", "application/geo+json");
-}
-
